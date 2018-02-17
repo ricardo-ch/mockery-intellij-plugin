d7b0363d3e8d127be25c8f725498e53e4115a82b
Giuseppe Landolfi <giuseppe.landolfi@ricardo.ch>
2018-02-17 17:02:58 +0100

    Add selection of interfaces to mock

--- src/ch/ricardo/plugins/intellij/mockery/action/GenMockeryActionHandler.java
+++ src/ch/ricardo/plugins/intellij/mockery/action/GenMockeryActionHandler.java
@@ -1,5 +1,6 @@
 package ch.ricardo.plugins.intellij.mockery.action;
 
+import com.intellij.codeInsight.hint.HintManager;
 import com.intellij.execution.ExecutionException;
 import com.intellij.execution.ExecutionHelper;
 import com.intellij.execution.ExecutionModes;
@@ -10,49 +11,78 @@ import com.intellij.execution.process.KillableColoredProcessHandler;
 import com.intellij.execution.process.OSProcessHandler;
 import com.intellij.execution.process.ProcessEvent;
 import com.intellij.execution.process.ProcessListener;
-import com.intellij.ide.projectView.ProjectView;
+import com.intellij.ide.util.ChooseElementsDialog;
 import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.editor.Caret;
 import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.editor.actionSystem.EditorWriteActionHandler;
+import com.intellij.openapi.editor.actionSystem.EditorActionHandler;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
-import com.intellij.openapi.ui.popup.JBPopupFactory;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Key;
-import com.intellij.openapi.vcs.changes.ui.ChangesTreeImpl;
 import com.intellij.openapi.vfs.VfsUtil;
-import com.intellij.openapi.vfs.VirtualFileManager;
 import com.intellij.util.EnvironmentUtil;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
+import java.util.StringJoiner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class GenMockeryActionHandler extends EditorWriteActionHandler {
+public class GenMockeryActionHandler extends EditorActionHandler {
+
+    private class InterfaceChooser extends ChooseElementsDialog<String> {
+
+        private InterfaceChooser (Project project, List<String> interfaces) {
+            super(project, interfaces, "Generate mockery", "Select the interfaces you want to generate mocks for", true);
+        }
+        @Override
+        protected String getItemText(String s) {
+            return s;
+        }
+
+        @Nullable
+        @Override
+        protected Icon getItemIcon(String s) {
+            return null;
+        }
+    }
+
+
     @Override
-    public void executeWriteAction(Editor editor, Caret caret, DataContext dataContext) {
+    public void doExecute(Editor editor, Caret caret, DataContext dataContext) {
 
         final StringBuilder out = new StringBuilder();
         final StringBuilder err = new StringBuilder();
 
-        String selectedText = editor.getSelectionModel().getSelectedText();
+        ArrayList<String> interfaces = new ArrayList<>();
+        for (String s : editor.getDocument().getText().split("\\r?\\n")) {
+            Pattern pattern = Pattern.compile("type (.*?) interface");
+            Matcher matcher = pattern.matcher(s);
+            if (matcher.find())
+            {
+                interfaces.add(matcher.group(1));
+            }
+        }
 
-        if (selectedText == null || selectedText.isEmpty()) {
+        HintManager.getInstance().showErrorHint(editor, "not supported");
 
-            JTextArea jTextArea = new JTextArea("You should select the interface you want to mock");
-            JComponent myPanel = new JPanel();
-            myPanel.add(jTextArea);
+        if (interfaces.isEmpty()) {
 
-            JBPopupFactory.getInstance()
-                    .createComponentPopupBuilder(myPanel, null)
-                    .setCancelOnClickOutside(true)
-                    .setRequestFocus(true)
-                    .setResizable(true)
-                    .setMayBeParent(true)
-                    .createPopup()
-                    .showInFocusCenter();
+            HintManager.getInstance().showErrorHint(editor, "No interfaces found");
+            return;
+        }
 
+        InterfaceChooser chooser = new InterfaceChooser(editor.getProject(), interfaces);
+        chooser.selectElements(interfaces);
+        List<String> selectedInterfaces = chooser.showAndGetResult();
+
+        if (selectedInterfaces.isEmpty()) {
             return;
         }
 
@@ -61,7 +91,12 @@ public class GenMockeryActionHandler extends EditorWriteActionHandler {
         GeneralCommandLine commandLine = PtyCommandLine.isEnabled() ? new PtyCommandLine() : new GeneralCommandLine();
         commandLine.withWorkDirectory(filePath.getParent().toString());
         commandLine.setExePath(EnvironmentUtil.getValue("GOPATH") + "/bin/mockery");
-        commandLine.addParameters("-name", selectedText);
+
+        StringJoiner joiner = new StringJoiner("|");
+        for (String s : selectedInterfaces) {
+            joiner.add("^"+s+"$");
+        }
+        commandLine.addParameters("-name", joiner.toString());
 
         OSProcessHandler processHandler;
 
@@ -95,7 +130,9 @@ public class GenMockeryActionHandler extends EditorWriteActionHandler {
             }
         });
         processHandler.startNotify();
-        ExecutionHelper.executeExternalProcess(editor.getProject(), processHandler, new ExecutionModes.BackGroundMode("mockery generation for " + selectedText), commandLine);
+        ExecutionHelper.executeExternalProcess(editor.getProject(), processHandler, new ExecutionModes.BackGroundMode("mockery generation"), commandLine);
 
     }
 }
+
+
